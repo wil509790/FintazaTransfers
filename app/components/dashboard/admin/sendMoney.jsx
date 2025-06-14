c1366835bd39093bda2720f248c0191f2019b60f
Hekmat <najafihekmat@gmail.com>
2025-06-14 19:02:29 +0500

    small design adjestment

--- app/components/dashboard/admin/sendMoney.jsx
+++ app/components/dashboard/admin/sendMoney.jsx
@@ -44,9 +44,9 @@ export default function SendMoney({ account }) {
   return (
     <div>
       <button
-        className=" rounded-md bg-indigo-600 px-2 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="rounded-lg bg-indigo-600 px-3 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all"
         onClick={() => setOpen(true)}
-      >{`Send Funds by ${account?.rtpSupport ? "RTP" : "ACH"}`}</button>
+      >{`Send ${account?.rtpSupport ? "RTP" : "ACH"}`}</button>
       <Modal open={open} setOpen={setOpen}>
         <form
           className="space-y-6 w-96"
